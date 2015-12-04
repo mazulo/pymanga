71f06e85a8ce823a9c484c7fa8d7e1948be5e1b9
Patrick Mazulo <pmazulo@gmail.com>
2015-12-03 22:25:28 -0300

    Set headers to get image file

--- pymanga.py
+++ pymanga.py
@@ -5,11 +5,26 @@ import time
 from bs4 import BeautifulSoup
 
 base_url = 'http://mangaop.info/capitulos/{}#1'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 \
+Firefox/42.0'
+HOST = 'mangas2014.centraldemangas.com.br'
+ACCEPT = 'image/png,image/*;q=0.8,*/*;q=0.5'
+ACCEPT_LANGUAGE = 'pt-BR,en;q=0.5'
+REFERER = 'http://mangaop.info/capitulos/747'
+headers = {
+    'User-Agent': USER_AGENT,
+    'Host': HOST,
+    'Accept': ACCEPT,
+    'Accept-Language': ACCEPT_LANGUAGE,
+    'Referer': REFERER
+}
 
 
 def download_chapter(chapter):
     url = base_url.format(chapter)
-    content = urllib.request.urlopen(url).read()
+    print(url)
+    req = urllib.request.Request(url)
+    content = urllib.request.urlopen(req).read()
 
     soup = BeautifulSoup(content, 'html.parser')
     words = soup.findAll('script')[16].string.split()
@@ -22,18 +37,24 @@ def download_chapter(chapter):
     manga_url = manga_url.replace('"', '')
     number_img = int(soup.find('select', id='capPages').text.split()[-1:][0])
 
-    print('number=', number_img)
+    print('Número de imagens:  ', number_img)
 
     final = ''
     for i in range(1, number_img+1):
         if i in range(1, 10):
             final = '0'+str(i)
-        print('final', final)
-        urllib.request.urlretrieve(
-            manga_url+'-{}.jpg'.format(final),
-            'capitulo-{}.jpg'.format(final)
-        )
-        time.sleep(10)
+            url = manga_url+'-{}.jpg'.format(final)
+            filename = 'capitulo-{}.jpg'.format(final)
+        else:
+            url = manga_url+'-{}.jpg'.format(i)
+            filename = 'capitulo-{}.jpg'.format(i)
+        print('Filename: ', filename)
+        print('URL: ', url)
+        time.sleep(5)
+        req = urllib.request.Request(url, headers=headers)
+        with open(filename, 'wb') as f:
+            content = urllib.request.urlopen(req).read()
+            f.write(content)
 
     print(manga_url, number_img)
     return
