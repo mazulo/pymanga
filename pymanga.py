260158fce05edcfc0036599f8e5cb5812919be24
Patrick Mazulo <pmazulo@gmail.com>
2015-12-04 18:29:49 -0300

    Finish the main feature

--- pymanga.py
+++ pymanga.py
@@ -4,59 +4,61 @@ import time
 
 from bs4 import BeautifulSoup
 
-base_url = 'http://mangaop.info/capitulos/{}#1'
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 \
-Firefox/42.0'
-HOST = 'mangas2014.centraldemangas.com.br'
-ACCEPT = 'image/png,image/*;q=0.8,*/*;q=0.5'
-ACCEPT_LANGUAGE = 'pt-BR,en;q=0.5'
-REFERER = 'http://mangaop.info/capitulos/747'
-headers = {
-    'User-Agent': USER_AGENT,
-    'Host': HOST,
-    'Accept': ACCEPT,
-    'Accept-Language': ACCEPT_LANGUAGE,
-    'Referer': REFERER
-}
+from configs import BASE_URL_ONE_PIECE, HEADER_ONE_PIECE
 
 
 def download_chapter(chapter):
-    url = base_url.format(chapter)
-    print(url)
+    # create the correct url with the chapter
+    url = BASE_URL_ONE_PIECE.format(chapter)
+    # create an abstraction of a URL Request
     req = urllib.request.Request(url)
+    # makes the request, and assign the content of the response in `content`
     content = urllib.request.urlopen(req).read()
-
+    # create the BeautifulSoup object from the content, using 'html.parse'
+    # to create the tree HTML
     soup = BeautifulSoup(content, 'html.parser')
+    # find the script tag that contains the image URL, then split in a list of
+    # strings
     words = soup.findAll('script')[16].string.split()
 
     manga_url = ''
     for word in words:
+        # check if word contains 'http' string
         if 'http' in word:
+            # if so, assign it to manga_url and break the for
             manga_url = word
             break
+    # remove "" from the manga_url
     manga_url = manga_url.replace('"', '')
+    # get the select tag that contains this id, split it and get the last value
     number_img = int(soup.find('select', id='capPages').text.split()[-1:][0])
 
     print('Número de imagens:  ', number_img)
 
     final = ''
     for i in range(1, number_img+1):
+        # check if i is between 1 and 9
         if i in range(1, 10):
+            # if so concatenate the 0 plus i
             final = '0'+str(i)
+            # concatenate this final with manga_url to create the URL image
             url = manga_url+'-{}.jpg'.format(final)
+            # create the filename
             filename = 'capitulo-{}.jpg'.format(final)
         else:
             url = manga_url+'-{}.jpg'.format(i)
             filename = 'capitulo-{}.jpg'.format(i)
-        print('Filename: ', filename)
-        print('URL: ', url)
-        time.sleep(5)
-        req = urllib.request.Request(url, headers=headers)
+        # create another request, this time to URL image using headers to this
+        # specific site
+        req = urllib.request.Request(url, headers=HEADER_ONE_PIECE)
         with open(filename, 'wb') as f:
+            # make request for the image
             content = urllib.request.urlopen(req).read()
+            # write the content of request into file
             f.write(content)
-
-    print(manga_url, number_img)
+        print('Arquivo salvo com sucesso! - {}'.format(filename))
+        # sleep for 5 seconds, to avoid problems
+        time.sleep(5)
     return
 
 
