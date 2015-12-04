bad9f367d0fcd1b279f933ecc998b9e9c8a56051
Patrick Mazulo <pmazulo@gmail.com>
2015-12-04 18:29:20 -0300

    Create config file that will contains headers informations

---
+++ configs.py
@@ -0,0 +1,10 @@
+BASE_URL_ONE_PIECE = 'http://mangaop.info/capitulos/{}#1'
+
+HEADER_ONE_PIECE = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 \
+                   Firefox/42.0',
+    'Host': 'mangas2014.centraldemangas.com.br',
+    'Accept': 'image/png,image/*;q=0.8,*/*;q=0.5',
+    'Accept-Language': 'pt-BR,en;q=0.5',
+    'Referer': 'http://mangaop.info/capitulos/747'
+}
