c4915fb2a12382a4c741acda4fb2865b7d642a63
Patrick Mazulo <pmazulo@gmail.com>
2015-12-27 23:35:33 -0300

    Update headers

--- conf.py
+++ conf.py
@@ -1,13 +1,15 @@
 BASE_URL_ONE_PIECE = 'http://mangaop.info/capitulos/{}#1'
 
 HEADER_ONE_PIECE = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 \
-                   Firefox/42.0',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                    (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
     'host': 'mangas2014.centraldemangas.com.br',
-    'accept': 'image/webp,image/*,*/*;q=0.8',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9, \
+              image/webp,*/*;q=0.8',
     'accept-encoding': 'gzip, deflate, sdch',
     'accept-language': 'pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4,es;q=0.2,fr; \
                        q=0.2,gl;q=0.2,nb;q=0.2,de;q=0.2',
+    'cache-control': 'max-age=0',
     'connection': 'keep-alive',
     'referer': 'http://mangaop.info/capitulos/747'
 }
